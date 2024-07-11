4511fb337c11c3d06294da9b1c6a16357c6a4ccf
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2024-07-11 20:28:16 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,24 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
+    val nivel: Nivel = Nivel.INTERMEDIARIO
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("O usuário ${usuario.nome} se matriculou na formação '$nome'")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val gusthawo = Usuario("Gusthawo Junkes")
+    val conteudos = mutableListOf(ConteudoEducacional("Kotlin Básico", 20), ConteudoEducacional("Kotlin Avançado", 40))
+    val formacao = Formacao("Bootcamp de Kotlin", conteudos)
+    formacao.matricular(gusthawo)
 }
